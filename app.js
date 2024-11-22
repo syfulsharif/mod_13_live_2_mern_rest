d0c9fce7b9e1fb873a7f282ade3f3dc11d50674b
syfulsharif <sharif.syful@gmail.com>
2024-11-22 12:34:40 +0600

    server updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = new express();
 const router = require('./src/route/api');
-const rateLimiter = require('express-rate-limiter');
+const rateLimiter = require('express-rate-limit');
 const helmet = require('helmet');
 const hpp = require('hpp');
 const cors = require('cors');
@@ -17,14 +17,29 @@ app.use(express.json({limit: '20mb'}));
 app.use(express.urlencoded({extended: true}));
 
 
-const limiter = new rateLimiter({windowMS: 15 * 60 * 1000, max: 3000});
+const limiter = rateLimiter({windowMs: 15 * 60 * 1000, max: 3000});
 
+// Database Connection
 let URL = "mongodb://localhost:27017/taskmern5";
 let OPTIONS = {user: "", pass: "", autoIndex: true};
-mongoose.connect(URL, OPTIONS, (err) => {
-    if (!err) {
-        console.log("MongoDB Connected");
-    } else {
-        throw err;
-    }
+// mongoose.connect(URL, OPTIONS, (err) => {
+//     if (!err) {
+//         console.log("MongoDB Connected");
+//     } else {
+//         throw err;
+//     }
+// })
+
+mongoose.connect(URL, OPTIONS).then(() => {
+    console.log("MongoDB Connected");
+}).catch((err) => {
+    console.log(err);
+});
+
+// Route Implementation
+app.use("/api", router);
+app.use("*", (req, res) => {
+    res.status(404).json({data: "Not Found"});
 })
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Happy developing ✨')
+const app = require('./app');
+
+app.listen(5080, () => {
+    console.log(`App listening on ${process.env.PORT || 5080}`);
+});
